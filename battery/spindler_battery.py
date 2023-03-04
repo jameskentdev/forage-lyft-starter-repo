035ae344cea87f0ceb4c643f71ede4927bdf1c3c
captainkent <29131214+captainkent@users.noreply.github.com>
2023-03-04 17:41:33 +0100

    Added battery package containing battery interface and its implementations.

---
+++ battery/spindler_battery.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from battery.battery import Battery
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date: datetime.date, current_date: datetime.date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self) -> bool:
+        return (self.current_date.year - self.last_service_date.year) >= 2
